180bdd8ccccccaee3b22e5bc2b11e1e784238228
frankyfrankfrank <adam@webadamfrank.com>
2016-07-11 14:44:36 -0400

    Added Friendly Front Pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,15 @@
                 <div class="panel-heading text-center"><h2>Book your Eby Estates Model Home VIP Appointment</h2></div>
 
                 <div class="panel-body">
-
+                  @if (Auth::user())
+                    <a class="btn btn-primary" href="/timeslots">View Timeslots</a>
+                  @else
+                    <p class="text-center">
+                    <a class="btn btn-primary" href="/login">Log In and Book</a>
+                    OR
+                    <a class="btn btn-primary" href="/register">Register</a>
+                    </p>
+                  @endif
                 </div>
             </div>
         </div>
