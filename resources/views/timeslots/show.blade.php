d7f607b050da60abf57f52e1125436e6f3beff6e
frankyfrankfrank <adam@webadamfrank.com>
2016-07-12 13:49:26 -0400

    Added Some Timeslot Logic

--- resources/views/timeslots/show.blade.php
+++ resources/views/timeslots/show.blade.php
@@ -3,13 +3,15 @@
 @section('content')
 
   <div class="container">
-        <div class="row timeslot">
+        <div class="row timeslot @if ($timeslot->is_assigned) timeslot-booked @endif">
 
           <div class="col-md-6">
             <h1>{{ $timeslot->time }}</h1>
-            <p>{{ $timeslot->agent }}</p>
 
           @if ($timeslot->is_assigned == 0)
+
+            <p>{{ $timeslot->agent }}</p>
+            
             {!! Form::model(
               $timeslot,
               [
@@ -30,7 +32,11 @@
             {!! Form::close() !!}
 
           @elseif($timeslot->is_assigned == 1)
-            <p>Booked</p>
+            <p>{{ $timeslot->agent }} with: <br>
+              John Doe<br>
+              1-519-555-1048<br>
+              john@example.com
+            </p>
           @endif
 
 
