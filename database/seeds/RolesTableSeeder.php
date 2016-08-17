19bdf329c1db1242b2a9db0dd02e8daba2015eed
frankyfrankfrank <adam@webadamfrank.com>
2016-08-17 10:45:33 -0400

    Added Role Seeder, Visitor and Admin

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Role::class)->create([
+          'name' => 'Agent',
+        ]);
+
+        factory(App\Role::class)->create([
+          'name' => 'Visitor',
+        ]);
+
+    }
+}
