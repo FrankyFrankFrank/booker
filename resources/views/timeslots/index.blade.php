f795bafc2d1dee9ba3303a163e74df4e69c46712
frankyfrankfrank <adam@webadamfrank.com>
2016-08-18 14:57:47 -0400

    Change appearance of timeslot index

--- resources/views/timeslots/index.blade.php
+++ resources/views/timeslots/index.blade.php
@@ -12,20 +12,14 @@
               <h2>Booked Timeslots</h2>
             </div>
           </div>
-
           @foreach ($unavailableTimeslotsByDay as $day => $timeslots)
             <div class="timeslot-day-row row">
-
               <div class=" col-md-12">
                 <h3>{{ date('l, F j', strtotime($day)) }}</h3>
               </div>
-
               @foreach($timeslots as $timeslot)
-
                 @include('timeslots.timeslot')
-
               @endforeach
-
             </div>
           @endforeach
         @endif
@@ -41,33 +35,27 @@
             <a href="/timeslots/create" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>&nbsp;&nbsp;Create New Timeslot</a>
           </div>
         @endif
+
         <br>
 
         @forelse ($availableTimeslotsByDay as $day => $timeslots)
           <div class="timeslot-day-row row">
-
             <div class=" col-md-12">
               <h3>{{ date('l, F j', strtotime($day)) }}</h3>
             </div>
-
             @foreach($timeslots as $timeslot)
-
               @include('timeslots.timeslot')
-
             @endforeach
           </div>
         @empty
           <div class="row">
             <div class="col-md-12">
               <div class="alert alert-info">
-                @if(Auth::user()->role == 'agent')All @else We're sorry. All @endif timeslots are currently booked.
-                </div>
+                @if(auth()->user()->hasRole('Agent'))All @else We're sorry. All @endif timeslots are currently booked.
               </div>
             </div>
+          </div>
         @endforelse
-
-
-
 
         </div>
       </div>
