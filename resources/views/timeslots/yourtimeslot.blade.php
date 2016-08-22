3892a2841dd2750b8e663edb55f63c077718aa94
frankyfrankfrank <adam@webadamfrank.com>
2016-08-22 10:23:49 -0400

    Added Cancel Appointment Modal

--- resources/views/timeslots/yourtimeslot.blade.php
+++ resources/views/timeslots/yourtimeslot.blade.php
@@ -11,14 +11,30 @@
 
         @include('timeslots.timeslotdetails')
 
-        {!! Form::open(['route' => ['unassign', $timeslot->id], 'method' => 'patch']) !!}
-        <button type="submit" class="btn btn-danger">
+
+        <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#confirm-cancel">
           <span class="glyphicon glyphicon-remove"></span> Cancel Your Appointment (Cannot be Undone)
         </button>
-        {!! Form::close() !!}
 
       </div>
     </div>
   </div>
+
+  {!! Form::open(['route' => ['unassign', $timeslot->id], 'method' => 'patch', 'id' => 'cancel-appointment']) !!}
+        <div class="modal fade"  id="confirm-cancel" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h3 class="model-title">Are you sure you want to cancel your appointment?</h3>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Go Back</button>
+                  <button type="submit" class="btn btn-primary">Cancel Appointment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+{!! Form::close() !!}
 
 @endsection
