a020265d22527feede564307121f08c656c3c332
frankyfrankfrank <adam@webadamfrank.com>
2016-08-22 10:42:40 -0400

    Procfile for Heroku

--- resources/views/timeslots/cancelled.blade.php
+++ resources/views/timeslots/cancelled.blade.php
@@ -6,7 +6,7 @@
   <div class="row">
     <div class="col-md-12">
 
-      <h1 class="text-center">You Have Successfully Cancelled Your Appointment.</h1>
+      <h1 class="text-center cancelled-message">You Have Successfully Cancelled Your Appointment.</h1>
       <p class="text-center"><a class="btn btn-primary" href="{{ url('/') }}">Finish</a> or <a class="btn btn-primary" href="{{ url('/timeslots') }}">View Available Timeslots</a></p>
 
     </div>
