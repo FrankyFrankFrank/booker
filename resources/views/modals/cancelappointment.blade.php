0947e9be7dccd5ec62c5dc9bcbdf1769fba60a84
Adam Frank <adam@webadamfrank.com>
2016-08-30 19:15:53 +0000

    fix for modals not booking correct timeslot

--- resources/views/modals/cancelappointment.blade.php
+++ resources/views/modals/cancelappointment.blade.php
@@ -1,16 +1,16 @@
 {!! Form::open(['route' => ['unassign', $timeslot->id], 'method' => 'patch', 'id' => 'cancel-appointment']) !!}
-      <div class="modal fade"  id="confirm-cancel" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h3 class="model-title">Are you sure you want to cancel your appointment?</h3>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Go Back</button>
-                <button type="submit" class="btn btn-primary">Cancel Appointment</button>
-            </div>
-          </div>
+  <div class="modal fade"  id="confirm-cancel" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h3 class="model-title">Are you sure you want to cancel your appointment?</h3>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Go Back</button>
+            <button type="submit" class="btn btn-primary">Cancel Appointment</button>
         </div>
       </div>
+    </div>
+  </div>
 {!! Form::close() !!}
