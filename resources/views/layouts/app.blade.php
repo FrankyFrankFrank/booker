51397794770d2f6e2d5dfe40da4bc28319cba87c
Adam Frank <adam@webadamfrank.com>
2016-08-30 17:43:46 +0000

    streamlined head and removed confusing file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,20 +10,10 @@
   {{-- Favicon --}}
   @include('layouts.favicon')
 
-  <!-- Fonts -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
-  <!-- Styles -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  {{-- jQuery --}}
+  <link rel="stylesheet" href="{{ url('css/app.css') }}">
+  <link rel="stylesheet" href="{{ url('css/style.css') }}">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-
+  <script src="{{ url('js/bootstrap.min.js') }}"></script>
 
 </head>
 <body id="app-layout">
@@ -80,9 +70,5 @@
 
   @yield('content')
 
-  <!-- JavaScripts -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-  {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
