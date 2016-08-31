fa720d9962320bd27be9a93b23c00f65364b420d
Adam Frank <adam@webadamfrank.com>
2016-08-31 16:14:00 +0000

    got rid of dumb conditional, just added a migration that makes a default project - can change with branch

---
+++ database/migrations/2016_08_31_121055_create_initial_project.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInitialProject extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      DB::table('projects')->insert([
+        'name' => 'Eby Estates',
+        'logo' => 'img/eby-estates-logo-header.png',
+        'main_color' => '#007f67',
+        'alt_color' => '#59C9A5',
+      ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
