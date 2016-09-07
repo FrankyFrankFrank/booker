4cb3a6ddf25dd4b9f95245386f9e2d23f082f352
frankyfrankfrank <adam@webadamfrank.com>
2016-09-07 15:11:57 -0400

    Test Create Agent Function Create New Timeslot

---
+++ tests/features/AgentFunctionsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AgentFunctionsTest extends TestCase
+{
+  use DatabaseMigrations;
+
+  public function testAgentCanCreateNewTimeslot()
+  {
+
+    $agent = factory(App\User::class)->create();
+    $agent->addRole('Agent');
+    $agent->roles()->detach(3);
+
+    $this->be($agent);
+
+    $this->visit('/timeslots/create')
+      ->type('2017-09-15', 'date')
+      ->select('13:30:00', 'time')
+      ->press('Submit');
+
+    $this->seePageIs('/timeslots')
+    ->see('Sep 15')
+    ->see('1:30 pm');
+  }
+
+}
