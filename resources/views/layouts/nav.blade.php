a5b8d00f62ce3b1582d7c0a239f4b4b7c15f8ce9
frankyfrankfrank <adam@webadamfrank.com>
2016-09-19 10:53:49 -0400

    Style Changes to Navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -10,8 +10,8 @@
         <span class="icon-bar"></span>
       </button>
 
-      <span class="navbar-brand" style="padding: 4px 18px;">
-        <svg style="vertical-align:middle;" width="40" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 43.64 50.94"><defs><style>.a{fill:#fff;}</style></defs><title>itsbooked-logo</title><path class="a" d="M15.32,16a.86.86,0,0,1,1-1h6.27c3.49,0,6,1.93,6,5.28A4.82,4.82,0,0,1,26,24.74v.06a5,5,0,0,1,3.4,5c0,3.9-3,5.89-6.68,5.89H16.29a.86.86,0,0,1-1-1Zm7.24,7.74a2.93,2.93,0,0,0,3-3.14,2.86,2.86,0,0,0-3.11-3H18.19v6.19Zm.29,9.47a3.31,3.31,0,0,0,3.58-3.52,3.36,3.36,0,0,0-3.58-3.55H18.19v7.06Z"/><path class="a" d="M21.83,50.94,0,38.2V12.74L21.83,0,43.64,12.73V38.21ZM1.33,37.44l20.49,12,20.48-12v-24l-20.48-12L1.33,13.5Z"/></svg> 
+      <span class="navbar-brand">
+        <svg style="vertical-align:middle;" width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 43.64 50.94"><defs><style>.a{fill:#fff;}</style></defs><title>itsbooked-logo</title><path class="a" d="M15.32,16a.86.86,0,0,1,1-1h6.27c3.49,0,6,1.93,6,5.28A4.82,4.82,0,0,1,26,24.74v.06a5,5,0,0,1,3.4,5c0,3.9-3,5.89-6.68,5.89H16.29a.86.86,0,0,1-1-1Zm7.24,7.74a2.93,2.93,0,0,0,3-3.14,2.86,2.86,0,0,0-3.11-3H18.19v6.19Zm.29,9.47a3.31,3.31,0,0,0,3.58-3.52,3.36,3.36,0,0,0-3.58-3.55H18.19v7.06Z"/><path class="a" d="M21.83,50.94,0,38.2V12.74L21.83,0,43.64,12.73V38.21ZM1.33,37.44l20.49,12,20.48-12v-24l-20.48-12L1.33,13.5Z"/></svg>
         it's BOOKED
       </span>
 
