208888eee3132e41bdf9edd2c0296441b58a2f4a
frankyfrankfrank <adam@webadamfrank.com>
2016-09-19 13:35:57 -0400

    Updated Favicon

--- resources/views/layouts/favicon.blade.php
+++ resources/views/layouts/favicon.blade.php
@@ -10,7 +10,7 @@
 <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicon/android-icon-192x192.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/favicon-96x96.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png?') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
 <link rel="manifest" href="{{ asset('favicon/manifest.json') }}">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
