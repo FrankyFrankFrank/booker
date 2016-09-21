1931bcde2b03d59da1808b7af49470935ae80b43
frankyfrankfrank <adam@webadamfrank.com>
2016-09-21 10:29:03 -0400

    timeslot creation form width

--- resources/views/timeslots/create.blade.php
+++ resources/views/timeslots/create.blade.php
@@ -7,19 +7,23 @@
 
           <h1>Create New Timeslot</h1>
 
-            @if(isset($timeslot))
-            <div class="alert alert-success" role="alert">
-              You've created a new timeslot on {{$timeslot->date}} at {{ $timeslot->time }}.
-            </div>
-            @endif
+          <div class="row">
+            <div class="col-md-6">
+              @if(isset($timeslot))
+                <div class="alert alert-success" role="alert">
+                  You've created a new timeslot on {{$timeslot->date}} at {{ $timeslot->time }}.
+                </div>
+              @endif
 
-            {!! Form::open(['url' => 'timeslots']) !!}
+              {!! Form::open(['url' => 'timeslots']) !!}
 
               @include('timeslots.form', ['submitButtonText' => 'Submit'])
 
-            {!! Form::close() !!}
+              {!! Form::close() !!}
 
-            @include('errors.list')
+              @include('errors.list')
+            </div>
+          </div>
 
 
 
