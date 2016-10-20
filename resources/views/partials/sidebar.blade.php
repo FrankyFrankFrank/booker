ea86e91e52cf8ae639ea6c662606d64f9e6292f7
frankyfrankfrank <adam@webadamfrank.com>
2016-10-20 08:44:26 -0400

    Logo Styling Sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="row top">
       <div class="col-md-12">
         <a href="{{ url('/') }}">
-          <img class="logo center-block" src="{{ asset($project->logo) }}" alt="{{ $project->name }}">
+          <img class="logo center-block img-responsive" src="{{ asset($project->logo) }}" alt="{{ $project->name }}">
         </a>
       </div>
     </div>
